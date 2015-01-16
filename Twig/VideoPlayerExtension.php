df30fc67266acd34c4be9ee4448e5aff6154772c
Thibault Harel <thibault.harel@gmail.com>
2015-01-16 12:03:05 +0100

    Add Twig extension to play the video
    Twig extension : {{ play_video( Video_object, width, height) }}

---
+++ Twig/VideoPlayerExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lifeinthecloud\VideoPlayerBundle\Twig;
+
+use Lifeinthecloud\VideoPlayerBundle\Service\VideoPlayerService;
+use Lifeinthecloud\VideoPlayerBundle\Model\Video;
+
+class VideoPlayerExtension extends \Twig_Extension
+{
+
+    protected $videoPlayerManager;
+
+
+    public function __construct(VideoPlayerService $videoPlayerManager = null)
+    {
+        $this->videoPlayerManager = $videoPlayerManager;
+    }
+    
+    public function renderVideo(Video $video, $videoWidth, $imageHeight)
+    {
+        return $this->videoPlayerManager->play(array(
+            'player' => array(
+                'width' => $videoWidth,
+                'height' => $imageHeight,
+            ),
+            'server' => array(
+                'server' => $video->getVideoServer()->getName(),
+                'id' => $video->getVideoId(),
+            ),
+        ));
+    }
+    
+    public function getFunctions()
+    {
+        return array(
+            'play_video' => new \Twig_Function_Method($this, 'renderVideo', array('is_safe' => array('html')))
+        );
+    }
+    
+    public function getName()
+    {
+        return 'lifeinthecloud_video_player.video_player_extension';
+    }
+
+}
